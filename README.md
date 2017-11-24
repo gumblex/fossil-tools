eb9236d9b0fee5c47f25a10ee30ba45529ba8691
Dingyuan Wang <abcdoyle888@gmail.com>
2017-11-25 00:53:48 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # fossil-tools
 Some tools for use with fossil scm.
+
+* no-initial-commit.patch: patch to disable initial commit in new repo.
+* pre-push: (.git/hooks) auto sync Fossil repo on git push. Needs above patch.
+* inotifycommit.sh: auto commit every change in the directory.
+
---
+++ inotifycommit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# apt install inotify-tools
+
+DIRNAME="${PWD##*/}"
+FOSSILREPO=".fossil"
+DATEFORMAT="+%Y-%m-%d %H:%M:%S"
+
+if [ ! -f "$FOSSILREPO" ]; then
+  fossil new "$FOSSILREPO"
+  fossil rebuild --wal "$FOSSILREPO"
+  fossil sqlite3 -R "$FOSSILREPO" "INSERT OR REPLACE INTO config VALUES ('project-name', '$DIRNAME', now());"
+  fossil open "$FOSSILREPO"
+fi
+
+while true; do
+    inotifywait --exclude _FOSSIL_ --exclude .fslckout --exclude "$FOSSILREPO*" -r -e create -e modify -e delete -e move .
+    # Check NGINX Configuration Test
+    # Only Reload NGINX If NGINX Configuration Test Pass
+    now="$(date "$DATEFORMAT" | tee /dev/tty)"
+    echo "$now"
+    msg="$now $(fossil addremove | tee /dev/tty | head -n 1)"
+    fossil commit -m "$msg"
+done
