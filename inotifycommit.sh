987b94e702f92eae469b12b4af6f543a23086276
Dingyuan Wang <abcdoyle888@gmail.com>
2017-11-27 01:18:38 +0800

    use real change time

--- inotifycommit.sh
+++ inotifycommit.sh
@@ -18,12 +18,14 @@ fi
 
 counter=0
 changed=0
+changedtime=""
 
 while true; do
     inotifywait --exclude "(_FOSSIL_|.fslckout|$FOSSILREPO.*)" -r -e close_write -e delete -e move -t 1 -q .
     exit_status=$?
     if [ $exit_status -eq 0 ]; then
         changed=1
+        changedtime="$(date "$DATEFORMAT" | tee /dev/tty)"
         counter=0
         continue
     elif [ $exit_status -eq 1 ]; then
@@ -34,8 +36,7 @@ while true; do
     if [ $changed -eq 1 ] && [ $counter -gt $WAITSEC ]; then
         counter=0
         changed=0
-        now="$(date "$DATEFORMAT" | tee /dev/tty)"
-        msg="$now $(fossil addremove | tee /dev/tty | head -n 1)"
+        msg="$changedtime $(fossil addremove | tee /dev/tty | head -n 1)"
         fossil commit --no-warnings --allow-fork -m "$msg"
     fi
 done
