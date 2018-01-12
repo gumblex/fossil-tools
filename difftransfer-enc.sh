e39333bcb39a0a67b161f3a4ea5ce05445407156
Dingyuan Wang <abcdoyle888@gmail.com>
2018-01-12 16:51:21 +0800

    fix encoding

--- difftransfer-enc.sh
+++ difftransfer-enc.sh
@@ -2,19 +2,21 @@
 tmpf=$(mktemp)
 fossil test-delta-create "$1" "$2" - | gzip -9 > "$tmpf"
 (sha1sum < "$tmpf" | awk '{print $1}'; base64 "$tmpf") | awk 'BEGIN {FS="";RS="\n"} {
-ln=""; last=""; lcount=1;
+ln=""; last=""; lstcnt="."; lcount=0;
 for(i = 1; i <= NF; i++) {
-  if ($i == last && lcount < 10) {
+  if ($i == last && lcount < 9) {
     lcount++;
   } else {
-    if (last == "\\") {ln=ln "\\\\"} else {ln=ln last};
-    if (lcount > 1) ln=ln"\\"(lcount-1);
-    last=$i;
-    lcount=1;
+    if (lstcnt == last) {ln=ln "\\."}
+    lstcnt="";
+    if (last == "\\") {ln=ln "\\0"; lstcnt="0";} else {ln=ln last}
+    if (lcount > 0) {ln=ln"\\"lcount; lstcnt=lcount}
+    last=$i; lcount=0;
   }
 }
-if (last == "\\") {ln=ln "\\\\"} else {ln=ln last};
-if (lcount > 1) ln=ln"\\"(lcount-1);
+if (lstcnt == last) {ln=ln "\\."}
+if (last == "\\") {ln=ln "\\0"} else {ln=ln last};
+if (lcount > 0) ln=ln"\\"lcount;
 print ln;
 }'
 rm "$tmpf"
